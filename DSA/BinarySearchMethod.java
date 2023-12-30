2f132348bd2fbf0fc299292b27132d6b148a06c0
anandkumar01 <vishwakarmaanand858@gmail.com>
2023-12-30 11:29:21 +0530

    Binary search with return type method

---
+++ DSA/BinarySearchMethod.java
@@ -0,0 +1,45 @@
+package DSA;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch1 {
+    public static int binarySearch(int[] arr, int low, int high, int searchElement) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == searchElement) {
+                return mid;
+            } else if (arr[mid] > searchElement) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1; // element not found
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number of elements in array:");
+            int size = sc.nextInt();
+            int[] arr = new int[size];
+            System.out.println("Enter elements of array: ");
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            if (sc.hasNextInt()) {
+                System.out.println("Enter element you want to search: ");
+                int searchElement = sc.nextInt();
+                int result = binarySearch(arr, 0, size - 1, searchElement);
+                if (result == -1) {
+                    System.out.println("Element is not present in the array.");
+                } else {
+                    System.out.println("Element is present at index: " + result);
+                }
+            } else {
+                System.out.println("Invalid input! Please enter an integer value.");
+            }
+        }
+    }
+}
