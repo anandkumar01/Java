3129ca0c0a61b911816e1d8d04c4490f3035c770
anandkumar01 <vishwakarmaanand858@gmail.com>
2023-12-30 20:12:18 +0530

    Selection sort program

---
+++ DSA/SelectionSort.java
@@ -0,0 +1,35 @@
+package DSA;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static int[] selectionSort(int[] arr, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] > arr[j]) {
+                    // swap arr[i] and arr[j]
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of the array: ");
+            int size = sc.nextInt();
+            int arr[] = new int[size];
+            System.out.println("Enter elements of the array: ");
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println("The unsorted array is: " + Arrays.toString(arr));
+            int result[] = selectionSort(arr, size);
+            System.out.print("The sorted array is: " + Arrays.toString(result));
+        }
+    }
+}
