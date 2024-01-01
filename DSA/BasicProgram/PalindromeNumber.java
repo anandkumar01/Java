e3151a3ad5d4ad7fb5376c93829bc570e7345374
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-01 12:00:29 +0530

    Checking palindrome number

--- DSA/BasicProgram/PalindromeNumber.java
+++ DSA/BasicProgram/PalindromeNumber.java
@@ -1,5 +1,26 @@
+// Write a program to check whether a given number is Palindrome or not.
+
 package DSA.BasicProgram;
 
+import java.util.Scanner;
+
 public class PalindromeNumber {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int num = sc.nextInt();
+        int k = num;
+        int rev = 0;
+        while(num>0){
+            int rem = num%10;
+            rev = rev*10 + rem;
+            num /= 10;
+        }
+        if(k == rev){
+            System.out.println("Given number is Palindrome.");
+        } else {
+            System.out.println("Given number is not Palindrome.");
+        }
+        sc.close();
+    }
 }
