8eebbf915e1d95134d8a2c2d7ff7cbafc46d0a3e
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-01 11:14:05 +0530

    occurence of digit in a number

--- DSA/BasicProgram/CountOfDigit.java
+++ DSA/BasicProgram/CountOfDigit.java
@@ -1,5 +1,27 @@
+// Problem : Write a program to count occurence of a digit in the given number.
+
 package DSA.BasicProgram;
 
+import java.util.Scanner;
+
 public class CountOfDigit {
-    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int num = sc.nextInt();
+            int number = num;
+            System.out.println("Enter a digit: ");
+            int digit = sc.nextInt();
+            
+            int count=0;
+            while(num!=0){
+                int rem = num%10;
+                if(rem==digit){
+                    count+=1;
+                }
+                num/=10;
+            }
+            System.out.println("Occurence of "+digit+" in "+number+" is: " + count);
+        }
+    }
 }
