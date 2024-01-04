c6dab0a4c95126c73589086fccdddf1d0e5fecaa
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-04 19:30:09 +0530

    Left rotation array by k steps

---
+++ DSA/Arrays/LeftRotationArray.java
@@ -0,0 +1,50 @@
+// Statement: Write a program to rotate the array to the left by k steps.
+// example:     arr = [1, 2, 3, 4, 5, 6, 7],  k=3
+
+package DSA.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LeftRotationArray {
+    public static void rotateArray(int[] arr, int n, int k){
+        // this will reverse the starting k elements in array.
+        reverseArray(arr, 0, k-1);                // ["3, 2, 1", 4, 5, 6, 7]
+        // this will reverse the last n-k elements in array.
+        reverseArray(arr, k, n-1);                      // [3, 2, 1, "7, 6, 5, 4"]
+        // this will reverse the complete array.
+        reverseArray(arr, 0, n-1);                // [4, 5, 6, 7, 1, 2, 3]
+    }
+
+    public static void reverseArray(int[] arr, int start, int end){
+        int index1=start, index2=end;
+        while (index1<index2) {
+            swap(arr, index1, index2);
+            index1++;
+            index2--;
+        }
+    }
+    
+    public static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter number of rotations: ");
+        int k = sc.nextInt();
+
+        rotateArray(arr, n, k);
+        System.out.println("Rotated array is: " + Arrays.toString(arr));
+        sc.close();
+    }
+}
