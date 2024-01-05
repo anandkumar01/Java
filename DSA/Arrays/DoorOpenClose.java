88be3d80ffd0a2ca360980286737210d316c5699
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-05 19:40:34 +0530

    Opening and closing doors using divisors

--- DSA/Arrays/DoorOpenClose.java
+++ DSA/Arrays/DoorOpenClose.java
@@ -2,6 +2,8 @@
               Your task is to simulate the opening and closing of doors based on the following pattern:
               1. For each pass, starting from the first door, you go through every ith door, where i is the pass number.
               2. If the door is closed, you open it and if it's open, you close it.
+        Trick: if count of divisors of the ith door is even then door will remains closed.
+               else door will be opened.
 */
 
 package DSA.Arrays;
@@ -12,26 +14,29 @@ import java.util.Scanner;
 
 public class DoorOpenClose {
     public static List<List<Integer>> doorOpenClose(int n) {
-        boolean[] res = new boolean[n];
         List<Integer> openDoor = new ArrayList<>();
         List<Integer> closeDoor = new ArrayList<>();
         for(int i=1; i <= n; i++) {
-            int k = 1;
-            while(k*i <= n) {
-                int index = k*i - 1;
-                res[index] = !res[index];
-                k++;
-            }
-            if(res[i-1]){
-                openDoor.add(i);
-            } else{
+            if(divisor(i)%2==0){
                 closeDoor.add(i);
+            } else{
+                openDoor.add(i);
             }
         }
         List<List<Integer>> result = new ArrayList<>();
         result.add(openDoor);
         result.add(closeDoor);
         return result;
+    }
+
+    public static int divisor(int n){
+        int ans=0;
+        for(int i=1; i<=n; i++) {
+            if(n%i==0){
+                ans+=1;
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
