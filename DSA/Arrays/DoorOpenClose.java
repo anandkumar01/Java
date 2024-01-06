049962546564921905c70bdf894a67cdda22babf
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-06 08:01:52 +0530

    Opening and closing of door using basic approach and divisors approach

--- DSA/Arrays/DoorOpenClose.java
+++ DSA/Arrays/DoorOpenClose.java
@@ -1,9 +1,9 @@
 /* Statement: You are given a row of doors, numbered from 1 to N. Initially, all the doors are closed.
               Your task is to simulate the opening and closing of doors based on the following pattern:
-              1. For each pass, starting from the first door, you go through every ith door, where i is the pass number.
+              1. For each pass, starting from the first door, you go through every multiple of ith door, where i is the pass number.
               2. If the door is closed, you open it and if it's open, you close it.
-        Trick: if count of divisors of the ith door is even then door will remains closed.
-               else door will be opened.
+        Trick: If count of divisors of the ith door is even then door will remains closed.
+               else door will remains opened.
 */
 
 package DSA.Arrays;
@@ -16,6 +16,24 @@ public class DoorOpenClose {
     public static List<List<Integer>> doorOpenClose(int n) {
         List<Integer> openDoor = new ArrayList<>();
         List<Integer> closeDoor = new ArrayList<>();
+
+        // -----Method 1-----
+    /* 
+        boolean[] res = new boolean[n];
+        for(int i=1; i <= n; i++) {
+            int k = 1;
+            while(k*i <= n) {
+                int index = k*i - 1;
+                res[index] = !res[index];
+                k++;
+            }
+            if(res[i-1]){
+                openDoor.add(i);
+            } else{
+                closeDoor.add(i);
+            }
+    */
+        // -----Method 2-----
         for(int i=1; i <= n; i++) {
             if(divisor(i)%2==0){
                 closeDoor.add(i);
@@ -23,6 +41,7 @@ public class DoorOpenClose {
                 openDoor.add(i);
             }
         }
+
         List<List<Integer>> result = new ArrayList<>();
         result.add(openDoor);
         result.add(closeDoor);
