e54829c5afacc191d527cca0ce2d5e3c3c0231aa
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-09 21:58:52 +0530

    Maximum subarray sum

--- DSA/Arrays/MaximumSubarray.java
+++ DSA/Arrays/MaximumSubarray.java
@@ -1,20 +1,16 @@
-// Statement: Given an integer array, find the subarray with the largest sum.
+// Statement: Given an integer array, find the largest sum of a subarray.
 
 package DSA.Arrays;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MaximumSubarray {
     public static int maximumSubarray(int[] arr, int n){
         int curSum = 0, maxSum = Integer.MIN_VALUE;
-        List<Integer> subarray = new ArrayList<>();
         for(int i=0; i<n; i++){
             curSum += arr[i];
             if(curSum>maxSum){
                 maxSum = curSum;
-                subarray.add(arr[i]);
             }
             if(curSum<0){
                 curSum = 0;
