e591918dda82ce1b62aae6e0cdba476bcd0529d5
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-15 15:29:21 +0530

    Binary search

--- DSA/SearchingAlgorithms/BinarySearchMethod.java
+++ DSA/SearchingAlgorithms/BinarySearchMethod.java
@@ -1,15 +1,16 @@
 package DSA.SearchingAlgorithms;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class BinarySearchMethod {
-    public static int binarySearch(int[] arr, int low, int high, int searchElement) {
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] == searchElement) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] == target) {
                 return mid;
-            } else if (arr[mid] > searchElement) {
+            } else if (arr[mid] > target) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -19,27 +20,22 @@ public class BinarySearchMethod {
     }
 
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the number of elements in array:");
-            int size = sc.nextInt();
-            int[] arr = new int[size];
-            System.out.println("Enter elements of array: ");
-            for (int i = 0; i < size; i++) {
-                arr[i] = sc.nextInt();
-            }
-            Arrays.sort(arr);
-            if (sc.hasNextInt()) {
-                System.out.println("Enter element you want to search: ");
-                int searchElement = sc.nextInt();
-                int result = binarySearch(arr, 0, size - 1, searchElement);
-                if (result == -1) {
-                    System.out.println("Element is not present in the array.");
-                } else {
-                    System.out.println("Element is present at index: " + result);
-                }
-            } else {
-                System.out.println("Invalid input! Please enter an integer value.");
-            }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter elements of array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
+        System.out.println("Enter element you want to search: ");
+        int target = sc.nextInt();
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element is not present in the array.");
+        } else {
+            System.out.println("Element is present at index: " + result);
+        }
+        sc.close();
     }
 }
