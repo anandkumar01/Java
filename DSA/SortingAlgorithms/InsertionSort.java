1cb790742cc53aa83a509f1438631ef756ca74c9
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-01-18 18:11:56 +0530

    Insertion sort algo

--- DSA/SortingAlgorithms/InsertionSort.java
+++ DSA/SortingAlgorithms/InsertionSort.java
@@ -4,25 +4,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class InsertionSort {
-    public static int[] insertionSort(int[] arr, int size){
-        for(int i=1; i<size; i++){
-            int key = arr[i];
-            int j = i-1;
-            while (j >= 0 && key < arr[j]){
-                arr[j+1] = arr[j];
-                j--;
+    public static int[] insertionSort(int[] arr, int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
             }
-            arr[j+1] = key;
         }
         return arr;
     }
-    public static void main(String[] args){
-        try(Scanner sc = new Scanner(System.in)){
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter the size of the array: ");
             int size = sc.nextInt();
             int arr[] = new int[size];
             System.out.println("Enter the elements of the array: ");
-            for(int i=0; i<size; i++){
+            for (int i = 0; i < size; i++) {
                 arr[i] = sc.nextInt();
             }
             System.out.println("The unsorted arry is: " + Arrays.toString(arr));
