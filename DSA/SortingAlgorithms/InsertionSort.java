f6241e48bf967a79d3f758b320deea440b9e9fba
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-08-13 17:46:08 +0530

    Insertion sort updated

--- DSA/SortingAlgorithms/InsertionSort.java
+++ DSA/SortingAlgorithms/InsertionSort.java
@@ -8,9 +8,7 @@ public class InsertionSort {
         for (int i = 0; i < size - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+                    swap(arr, j, j - 1);
                 } else {
                     break;
                 }
@@ -19,18 +17,24 @@ public class InsertionSort {
         return arr;
     }
 
+    public static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the size of the array: ");
-            int size = sc.nextInt();
-            int arr[] = new int[size];
-            System.out.println("Enter the elements of the array: ");
-            for (int i = 0; i < size; i++) {
-                arr[i] = sc.nextInt();
-            }
-            System.out.println("The unsorted arry is: " + Arrays.toString(arr));
-            int result[] = insertionSort(arr, size);
-            System.out.println("The sorted array is: " + Arrays.toString(result));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
+        System.out.println("The unsorted arry is: " + Arrays.toString(arr));
+        int result[] = insertionSort(arr, size);
+        System.out.println("The sorted array is: " + Arrays.toString(result));
+        sc.close();
     }
 }
