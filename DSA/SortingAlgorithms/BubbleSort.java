9ca42f98a643fea7f03697fb51884aafbdff0858
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-08-13 17:28:40 +0530

    Bubble sort  updated

--- DSA/SortingAlgorithms/BubbleSort.java
+++ DSA/SortingAlgorithms/BubbleSort.java
@@ -5,21 +5,28 @@ import java.util.Scanner;
 
 public class BubbleSort {
     public static int[] bubbleSort(int[] arr, int size) {
-        for (int i = 0; i < size - 1; i++) {
-            boolean flag = true; // Flag is to check if any swapping has been performed or not in one pass.
-            for (int j = 0; j < size - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    flag = false;
+        boolean swapped;
+        for (int i = 0; i < size; i++) {
+            // swapped is to check if any swapping is performed in one pass.
+            swapped = false;
+            for (int j = 1; j < size - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                    swapped = true;
                 }
             }
-            if (flag) {
+            // No swapping means array is already sorted
+            if (!swapped) {
                 break;
             }
         }
         return arr;
+    }
+
+    public static void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 
     public static void main(String[] args) throws Exception {
