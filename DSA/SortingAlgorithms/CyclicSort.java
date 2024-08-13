03152fd00c372c391fd7e89c95c3931517f7d9a3
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-08-13 17:42:11 +0530

    Cyclic sort updated

--- DSA/SortingAlgorithms/CyclicSort.java
+++ DSA/SortingAlgorithms/CyclicSort.java
@@ -6,24 +6,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class CyclicSort {
-    public static int[] cyclicSort(int[] arr, int size){
+    public static int[] cyclicSort(int[] arr, int size) {
         int i = 0;
-        while(i < size){
+        while (i < size) {
             int correctIndex = arr[i] - 1;
-            if(arr[i] != arr[correctIndex]){
+            if (arr[i] != arr[correctIndex]) {
                 swap(arr, i, correctIndex);
-            } else{
+            } else {
                 i++;
             }
         }
         return arr;
     }
-    public static void swap(int[] arr, int index1, int index2){
+
+    public static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
-    
+
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array: ");
