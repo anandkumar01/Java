66906b1d3afc623bcadbd996d22085b91987b84e
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-08-14 17:58:27 +0530

    Count occurence of a digit

--- DSA/BasicProgram/CountOfDigit.java
+++ DSA/BasicProgram/CountOfDigit.java
@@ -6,22 +6,22 @@ import java.util.Scanner;
 
 public class CountOfDigit {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter a number: ");
-            int num = sc.nextInt();
-            int number = num;
-            System.out.println("Enter a digit: ");
-            int digit = sc.nextInt();
-            
-            int count=0;
-            while(num!=0){
-                int rem = num%10;
-                if(rem==digit){
-                    count+=1;
-                }
-                num/=10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int number = num;
+        System.out.println("Enter a digit: ");
+        int digit = sc.nextInt();
+
+        int count = 0;
+        while (num != 0) {
+            int rem = num % 10;
+            if (rem == digit) {
+                count += 1;
             }
-            System.out.println("Occurence of "+digit+" in "+number+" is: " + count);
+            num /= 10;
         }
+        System.out.println("Occurence of " + digit + " in " + number + " is: " + count);
+        sc.close();
     }
 }
