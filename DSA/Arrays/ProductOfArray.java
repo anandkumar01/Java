4fd541fb9be99a6b9559c6339ead3dc5425fc8c0
anandkumar01 <vishwakarmaanand858@gmail.com>
2024-08-26 21:13:43 +0530

    Product of all integers of array except itself

---
+++ DSA/Arrays/ProductOfArray.java
@@ -0,0 +1,41 @@
+// Statement: Write a program to return an array of product of all integers except itself.
+
+package DSA.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ProductOfArray {
+
+    public static int[] productExceptSelf(int[] arr, int n) {
+        int[] res = new int[n];
+
+        int product = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = product;
+            product *= arr[i];
+        }
+
+        product = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= product;
+            product *= arr[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] result = productExceptSelf(arr, n);
+        System.out.println(Arrays.toString(result));
+        sc.close();
+    }
+}
